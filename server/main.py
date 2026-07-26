e548c63c7bb0fb5d9410f605a6ca9b7fa54712c8
YabseraBogale <yabserapython@gmail.com>
2026-07-26 16:37:40 +0300

    fixing sync

--- server/main.py
+++ server/main.py
@@ -1,11 +1,23 @@
-from flask import Flask,url_for
+from os import stat_result
 
-app=Flask(__name__)
+from flask import Flask,url_for,jsonify
+import sqlite3
 
+app = Flask(__name__)
+db = sqlite3.connect("database.db",check_same_thread=False)
+pointer = db.cursor()
 
-@app.route("/")
-def index():
-    return "ok"
+@app.route("/<starting_date,end_date>")
+def search_all(starting_date,end_date):
+    statment="""    
+    SELECT Name,ClosePrice,SUM(Volume) AS TotalDailyVolume
+    FROM TranscationtHistory
+    WHERE DatePrice = '?' BETWEEN '?'
+    GROUP BY Name, DatePrice;
+    """
+    pointer.execute(statment,(starting_date,end_date))
+    result=pointer.fetchall()
+    return jsonify(result)
 
 if __name__=="__main__":
     app.run(debug=True)
