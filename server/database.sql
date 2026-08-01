919f6269894c622a73c83b3f62ad6e32127e0c63
YabseraBogale <yabserapython@gmail.com>
2026-08-01 20:55:38 +0300

    fixing query

--- server/database.sql
+++ server/database.sql
@@ -20,13 +20,23 @@ CREATE TABLE IF NOT EXISTS Account(
     AllFee float not null 
 )
 
-SELECT Name,ClosePrice, SUM(Volume) AS TotalDailyVolume
+SELECT Name, ClosePrice, SUM(Volume) AS TotalDailyVolume
 FROM TranscationtHistory
 WHERE DatePrice = '?' AND '?'
 GROUP BY Name, DatePrice;
+
+SELECT Name,SUM(Volume * ClosePrice) AS TotalDailyVolume
+FROM TranscationtHistory
+WHERE DatePrice = '?' AND '?'
+GROUP BY Name, DatePrice;
+
+SELECT Sum(Volume * ClosePrice) 
+FROM TranscationtHistory
+WHERE Name = '?'
+  AND DatePrice BETWEEN '?' AND '?';
+
 
 SELECT Sum(Volume) 
 FROM TranscationtHistory
 WHERE Name = '?'
   AND DatePrice BETWEEN '?' AND '?';
-
