f8d577e5ef71196de73e954e788396dd0e4845af
YabseraBogale <yabserapython@gmail.com>
2026-08-09 19:18:01 +0300

    ...fixing query

--- esx/database.sql
+++ esx/database.sql
@@ -3,15 +3,15 @@ CREATE TABLE IF NOT EXISTS Company(
     Name Text Not Null,
     PublicShare Integer Not Null,
     ClosedShare Integer Not Null,
-    SharePrice Float Not Null,
-)
+    SharePrice Float Not Null
+);
 
 CREATE TABLE IF NOT EXISTS Account(
     AcountID Integer PRIMARY KEY,
     StartingMoney Float NOT Null,
     TotalValue Float NOT Null,
-    AllFee float not null 
-)
+    AllFee Float Not Null 
+);
 
 CREATE TABLE IF NOT EXISTS BuyOrder(
     Symbol Text References Transcation,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS BuyOrder(
     NumberOfShares Integer NOT Null,
     BoughtPrice Float NOT Null,
     BoughtDate Date Not Null
-)
+);
 
 CREATE TABLE IF NOT EXISTS SellOrder(
     Symbol Text References Transcation,
@@ -27,4 +27,4 @@ CREATE TABLE IF NOT EXISTS SellOrder(
     NumberOfShares Integer NOT Null,
     SellPrice Float NOT Null,
     SoldDate Date Not Null
-)
+);
